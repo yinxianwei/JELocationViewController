50ae4ffae3c41e144975c678169d8699704cd3fe
尹现伟 <yinxianwei@qq.com>
2014-08-25 17:26:16 +0800

    定位展示周边信息

---
+++ JELocationViewController/JEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JEAppDelegate.h
+//  JELocationViewController
+//
+//  Created by 尹现伟 on 14-8-25.
+//  Copyright (c) 2014年 DNE Technology Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JELocationViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JELocationViewController
+//
+//  Created by 尹现伟 on 14-8-25.
+//  Copyright (c) 2014年 DNE Technology Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JEAppDelegate class]));
+    }
+}
